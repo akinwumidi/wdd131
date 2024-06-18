57e9e89c6071217a9d8727f419b80387f3c95876
Akinwumidi <israelakin14@gmail.com>
2024-06-19 00:21:33 +0100

    core update: optimized temple homepage for mobile screens

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,9 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const nav_mobile = document.querySelector(".nav-mobile");
-const backdrop =document.querySelector(".backdrop")
+const backdrop = document.querySelector(".backdrop")
 
 const handleMobileNav =()=>{
-hamburger.classList.toggle("transform-menu")
+    hamburger.classList.toggle("transform-menu")
     nav_mobile.classList.toggle("show-nav-mobile")
     backdrop.classList.toggle("show-backdrop")
 }
